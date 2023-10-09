4f09758a4593ba10abcacc8653c55bf0b9114cbf
Gou Nakamura <nakamura462645@gmail.com>
2023-10-09 14:43:16 +0900

    installed react and react-dom

--- public/js/cljs-runtime/triangle.core.js
+++ public/js/cljs-runtime/triangle.core.js
@@ -1,6 +1,18 @@
 goog.provide('triangle.core');
-triangle.core.init = (function triangle$core$init(){
-return cljs.core.prn.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2(["hello world"], 0));
+var module$node_modules$react_dom$client=shadow.js.require("module$node_modules$react_dom$client", {});
+triangle.core.main = (function triangle$core$main(){
+return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),"hello world"], null);
 });
+if((typeof triangle !== 'undefined') && (typeof triangle.core !== 'undefined') && (typeof triangle.core.root !== 'undefined')){
+} else {
+triangle.core.root = module$node_modules$react_dom$client.createRoot(document.getElementById("root"));
+}
+triangle.core.render_main = (function triangle$core$render_main(){
+return reagent.core.create_class.cljs$core$IFn$_invoke$arity$1(new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"component-name","component-name",-1318676056),"my-main",new cljs.core.Keyword(null,"reagent-render","reagent-render",-985383853),triangle.core.main], null));
+});
+triangle.core.init = (function triangle$core$init(){
+return triangle.core.root.render(reagent.core.as_element.cljs$core$IFn$_invoke$arity$1(new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [triangle.core.render_main], null)));
+});
+triangle.core.init();
 
 //# sourceMappingURL=triangle.core.js.map
--- src/main/triangle/core.cljs
+++ src/main/triangle/core.cljs
@@ -1,3 +1,18 @@
-(ns triangle.core)
+(ns triangle.core
+  (:require  [reagent.core :as r]
+             [reagent.dom :as rdom]
+             ["react-dom/client" :refer [createRoot]]))
 
-(defn init [] (prn "hello world"))
+(defn main []
+  [:div "hello world"])
+
+(defonce root (createRoot (js/document.getElementById "root")))
+
+(defn render-main []
+  (r/create-class
+   {:component-name "my-main"
+    :reagent-render main}))
+
+(defn init []  (.render root (r/as-element [render-main])))
+
+(init)
